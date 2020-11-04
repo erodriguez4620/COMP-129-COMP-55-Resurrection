63746281097664064bc2551f003ce6fc0d3f9943
rando <e_rodriguez34@u.pacific.edu>
2020-11-03 19:16:16 -0800

    Finished up EnemyType enums

--- group-project/src/starter/EnemyType.java
+++ group-project/src/starter/EnemyType.java
@@ -1,5 +1,13 @@
 package starter;
 
 public enum EnemyType {
-
+GOBLIN, SLIME, BOSS;
+	public String getString() {
+		switch (this) {
+		case GOBLIN: return "goblin";
+		case SLIME: return "slime";
+		case BOSS: return "boss";
+		}
+		return "n/a";
+	}
 }
