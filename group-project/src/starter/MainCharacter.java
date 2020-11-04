f5973765469d8d4366ee3ea6a0a02f97b6aaf5bc
Shado <a_necoechea1@u.pacific.edu>
2020-11-04 10:42:37 -0800

    Got the ball rolling for MainCharacter, not sure if it's all that we need.

--- group-project/src/starter/MainCharacter.java
+++ group-project/src/starter/MainCharacter.java
@@ -1,5 +1,43 @@
 package starter;
 
 public class MainCharacter {
+	public int playerHP; //player's HP
+	private ItemType item; //what item the player is holding
+	Space player; //where the player will be
+	
+	
+	 public MainCharacter(ItemType item, int playerHP, Space player) {
+		this.item = item;
+		this.playerHP = playerHP;
+		this.player = player;
+	}
+
+	//Getters
+	public int getPlayerHP() {
+		return playerHP;
+	}
+
+	public ItemType getItem() {
+		return item;
+	}
+	
+	public Space getPlayer() {
+		return player;
+	}
+
+	
+	//Setters
+	public void setPlayerHP(int playerHP) {
+		this.playerHP = playerHP;
+	}
+	
+	public void setItem(ItemType item) {
+		this.item = item;
+	}
+
+	public void setPlayer(Space player) {
+		this.player = player;
+	}
+	
 	
 }
