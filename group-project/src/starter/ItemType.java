986ab54d4da53c166616bc20c0cb63c234037d80
Joseph <j_kopania@u.pacific.edu>
2020-11-03 19:18:21 -0800

    ItemType class created

--- group-project/src/starter/ItemType.java
+++ group-project/src/starter/ItemType.java
@@ -1,5 +1,15 @@
 package starter;
 
 public enum ItemType {
-
+	WEAPON, POTION, KEY;
+	
+	public String toString() {
+		switch(this) {
+			case WEAPON: return "weapon";
+			case POTION: return "potion";
+			case KEY: return "key";
+		}
+		return "n/a";
+	}
+	
 }
