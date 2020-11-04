d8cd6bf859b9daeccd9e6957168256534805416f
rando <e_rodriguez34@u.pacific.edu>
2020-11-03 19:45:43 -0800

    Finished the Item class.

--- group-project/src/starter/Item.java
+++ group-project/src/starter/Item.java
@@ -1,5 +1,23 @@
 package starter;
 
 public class Item {
-
+	int valuePoint; 
+	ItemType itemType;
+	
+	public Item(ItemType itemType, int valuePoint) {
+		this.itemType = itemType;
+		this.valuePoint = valuePoint;
+	}
+	
+	public String toString() {
+		return itemType.toString();
+	}
+	
+	public void setValuePoint(int valuePoint) {
+		this.valuePoint = valuePoint;
+	}
+	
+	public int getValuePoint() {
+		return valuePoint;
+	}
 }
